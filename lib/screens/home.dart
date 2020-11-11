71ddf659b94665a32c6438534112d741b95a0450
Simran Nijhar <simran.nijhar@gmail.com>
2020-11-11 19:28:56 +0800

    commit

--- lib/screens/home.dart
+++ lib/screens/home.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:ielts_app/screens/adminMenu.dart';
+import 'package:ielts_app/screens/testTakerMenu.dart';
 import 'package:ielts_app/widgets/title.dart';
 
 class Home extends StatefulWidget {
@@ -7,6 +9,21 @@ class Home extends StatefulWidget {
 }
 
 class _HomeState extends State<Home> {
+
+  adminMenu(){
+    Navigator.pushReplacement(context, MaterialPageRoute(
+        builder: (context) => AdminMenu()
+    ));
+  }
+
+  testTakerMenu(){
+    Navigator.pushReplacement(context, MaterialPageRoute(
+      builder: (context) => TestTakerMenu()
+    ));
+  }
+
+
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -21,22 +38,29 @@ class _HomeState extends State<Home> {
               child: Column(
                 children: [
                   Spacer(),
-                  Container(
-                    padding: EdgeInsets.symmetric(vertical: 10),
-                    decoration: BoxDecoration(
-                      color: Colors.black38,
-                      borderRadius: BorderRadius.circular(25),
-                    ),
-                    alignment: Alignment.center,
-                    width: MediaQuery.of(context).size.width - 48,
-                    child: Text(
-                      "Admin",
-                      style: TextStyle(color: Colors.black, fontSize: 20),
+                  //Button
+                  GestureDetector(
+                    onTap: (){
+                      adminMenu();
+                    },
+                    child: Container(
+                      padding: EdgeInsets.symmetric(vertical: 10),
+                      decoration: BoxDecoration(
+                        color: Colors.black38,
+                        borderRadius: BorderRadius.circular(25),
+                      ),
+                      alignment: Alignment.center,
+                      width: MediaQuery.of(context).size.width - 48,
+                      child: Text(
+                        "Admin",
+                        style: TextStyle(color: Colors.black, fontSize: 20),
+                      ),
                     ),
                   ),
                   SizedBox(
                     height: 50,
                   ),
+                  //Button
                   Container(
                     padding: EdgeInsets.symmetric(
                       vertical: 10,
