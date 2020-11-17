610a014dd9d82d0a69b4c9fbb4abda3c49f1fb20
Simran Nijhar <simran.nijhar@gmail.com>
2020-11-17 16:14:36 +0800

    commit

--- lib/database/database.dart
+++ lib/database/database.dart
@@ -1,10 +1,13 @@
-import 'package:cloud_firestore/cloud_firestore.dart';
+import "package:cloud_firestore/cloud_firestore.dart";
 
 class DatabaseService {
   Future<void> addTest(Map testMap, String testID) async {
+    // ignore: deprecated_member_use
     await Firestore.instance
         .collection("Test")
+        // ignore: deprecated_member_use
         .document(testID)
+        // ignore: deprecated_member_use
         .setData(testMap)
         .catchError((e) {
       print(e.toString());
--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,9 @@
 import 'package:flutter/material.dart';
 import 'package:ielts_app/screens/home.dart';
 
-void main() {
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp();
   runApp(MyApp());
 }
 
